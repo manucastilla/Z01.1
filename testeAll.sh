ab86696075ad0a81f07305ffc8685e7cc4f7d681
lucaslealvale <lucaslealvale01@gmail.com>
2019-11-21 06:54:36 -0300

    testeAll

---
+++ testeAll.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./Projetos/C-LogicaCombinacional/testeLogicaCombinacional.py
+./Projetos/D-UnidadeLogicaAritmetica/testeULA.py
+./Projetos/E-LogicaSequencial/testeLogicaSequencial.py
+./Projetos/F-Assembly/testeAssembly.py 
+./Projetos/G-Computador/testeAssemblyMyCPU.py 
+./Projetos/G-Computador/testeHW.py
+./Projetos/H-Assembler/genJAR.py
+./Projetos/I-VM/testeVm.py
+
+
+
+
